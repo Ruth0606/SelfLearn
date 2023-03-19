d3e09fd0d0700d52d563a398539acaeacb67afce
Ruth <ruth6780606@gmail.com>
2023-03-19 17:02:09 +0200

    19/03

--- client/src/Components/Tirgul/SelectMaterial.js
+++ client/src/Components/Tirgul/SelectMaterial.js
@@ -1,66 +1,193 @@
 import React, { useEffect, useState } from "react";
-import { Dropdown } from 'primereact/dropdown';
-import TirgulQ from './TirgulQ';
-import useGetData from '../../Hooks/useGetData'
+import { Dropdown } from "primereact/dropdown";
+import TirgulQ from "./TirgulQ";
+import useGetData from "../../Hooks/useGetData";
 import GetPageQ from "./GetPageQ";
-
+import { useDataFunctions } from "../../Hooks/useDataFunctions";
 
 export default function SelectMaterial() {
-    let idlevel=null;
-    const [flagClass, setflagClass] = useState(true);
-    const [flagSubject, setflagSubject] = useState(true);
-    const [flagSubsubject, setflagSubsubject] = useState(true);
+  const { getDataFunc } = useDataFunctions();
 
-    const [selectedClass, setSelectedClass] = useState(null);
-    const [selectedSubject, setSelectedSubject] = useState(null);
-    const [selectedSubsubject, setSelectedSubsubject] = useState(null);
-    const [selectedlevel, setSelectedlevel] = useState(null);
+  let idlevel = null;
+  const [flagClass, setflagClass] = useState(true);
+  const [flagSubject, setflagSubject] = useState(true);
+  const [flagSubsubject, setflagSubsubject] = useState(true);
 
-    const [f, setf] = useState(false);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [selectedSubsubject, setSelectedSubsubject] = useState(null);
+  const [selectedlevel, setSelectedlevel] = useState(null);
 
-    //useEffect(()=>setf(true),[selectedlevel])
+  const [arrsubjects, setArrsubjects] = useState([]);
+  const [arrsubsubjects, setArrsubsubjects] = useState([]);
+  const [arrlevels, setArrlevels] = useState([]);
+  const [f, setf] = useState(false);
 
-    const classes = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
-    var c=[];
-//     const{data,error,loading,refetch}=  useGetData("http://localhost:8000/data/class");
+  //useEffect(()=>setf(true),[selectedlevel])
 
-    
-//   useEffect(()=>{console.log("data",data);},[data]);
+  const classes = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+  const classes2 = [
+    { name: "b", code: "NY" },
+    { name: "d", code: "RM" },
+    { name: "g", code: "LDN" },
+    { name: "i", code: "IST" },
+    { name: "o", code: "PRS" },
+  ];
+  //let arrclasses = [];
+ // let arrsubjects = [];
+  const {
+    data: dataClass,
+    error,
+    loading,
+    refetch,
+  } = useGetData("http://localhost:8000/data/class");
+
+  //     useGetData("http://localhost:8000/data/class").then((data)=>{
+  //     //console.log(data)
+  //     data.forEach(element => {   //   console.log(element);
+  //         arrclasses.push({"description":element.description,"id":element.idclass})
+  //     });
+  //    // arrclasses=data;
+  //     console.log("arrclasses",arrclasses)
+  //     });
+  //     // console.log(classes2)
+
+  useEffect(() => {
+    console.log("selectedClass", selectedClass);
+    if (selectedClass != null) {
+      console.log("selectedClass", selectedClass);
+      console.log("dataClass", dataClass);
+
+      //   const arr = dataClass.filter(
+      //     (el) => el.description == selectedClass.description
+      //   );
+      //   const idclass = arr[0].idclass;
+      const idclass = selectedClass.idclass;
+      getDataFunc(`http://localhost:8000/data/subject/${idclass}`).then(
+        (data) => {
+          console.log(data);
+          setArrsubjects (data) ;
+          // data.forEach(element => {
+          //     arrsubjects.push({"description":element.description,"id":element.id})
+          // });
+          console.log("arrsubjects", arrsubjects);
+        }
+      );
+      //const { data: dataSubject, error,  loading,refetch,} = useGetData(`http://localhost:8000/data/subject/${idclass}`);
+
+      // const arr=arrclasses.filter((el)=>el.description==selectedClass.description);
+      // console.log(arrclasses)
+      // console.log(arr)
+      // const idclass=arr[0].id;
+      // getDataFunc(`http://localhost:8000/data/subject/${idclass}`).then((data)=>{
+      //     console.log(data)
+      //     arrsubjects=data;
+      //     // data.forEach(element => {
+      //     //     arrsubjects.push({"description":element.description,"id":element.id})
+      //     // });
+      //     console.log(arrsubjects)
+      //     });
+    }
+  }, [selectedClass]);
+
+  useEffect(() => {
+    if (selectedSubject != null) {
+      const idsubject = selectedSubject.idsubject;
+      getDataFunc(`http://localhost:8000/data/subsubject/${idsubject}`).then(
+        (data) => {
+          console.log(data);
+          setArrsubsubjects (data) ;
+          console.log("setArrsubsubjects", arrsubsubjects);
+        }
+      );
+    }
+  }, [selectedSubject]);
 
 
-    useGetData("http://localhost:8000/data/class").then((data)=>{
-    console.log(data)
-    data.forEach(element => {
-    c.push({"name":element.description,"code":element.description}) 
-    });
-    console.log(c)
-    });
+  
+  useEffect(() => {
+    if (selectedSubsubject != null) {
+      const idsubsubject = selectedSubsubject.idsubsubject;
+      getDataFunc(`http://localhost:8000/data/level/${idsubsubject}`).then(
+        (data) => {
+          console.log(data);
+          setArrlevels (data) ;
+          console.log("setArrsubsubjects", arrsubsubjects);
+        }
+      );
+    }
+  }, [selectedSubsubject]);
+  
 
-    return (
-        <div className="card flex justify-content-center" style={{"margin":"3%"}}>
-            <Dropdown value={selectedClass} onChange={(e) => {setSelectedClass(e.value);setflagClass(false) }} options={c} optionLabel="name" 
-                placeholder="בחר כיתה" className="p-invalid w-full md:w-14rem"  style={{"marginLeft":"3%","marginRight":"3%"}} />
-            <Dropdown disabled={flagClass} value={selectedSubject} onChange={(e) => {setSelectedSubject(e.value);setflagSubject(false) }} options={classes} optionLabel="name" 
-                placeholder="בחר מקצוע" className="p-invalid w-full md:w-14rem"  style={{"marginLeft":"3%","marginRight":"3%"}} />
-            <Dropdown disabled={flagSubject} value={selectedSubsubject} onChange={(e) =>{ setSelectedSubsubject(e.value);setflagSubsubject(false)}} options={classes} optionLabel="name" 
-                placeholder="בחר נושא" className="p-invalid w-full md:w-14rem"  style={{"marginLeft":"3%","marginRight":"3%"}} />
-            <Dropdown disabled={flagSubsubject} value={selectedlevel} onChange={(e) =>{setSelectedlevel(e.value);setf(true);idlevel=e.value.idlevel}} options={classes} optionLabel="name" 
-                placeholder="בחר רמה" className="p-invalid w-full md:w-14rem"  style={{"marginLeft":"3%","marginRight":"3%"}} />
-            {f&&<GetPageQ idlevel={idlevel}></GetPageQ>}
-           
-        </div>
-        
-          
-        
-    )
+  return loading ? (
+    <div>loading</div>
+  ) : (
+    <div className="card flex justify-content-center" style={{ margin: "3%" }}>
+      {dataClass.length && (
+        <Dropdown
+          value={selectedClass}
+          onChange={(e) => {
+            setSelectedClass(e.value);
+            setflagClass(false);
+            console.log(e.value);
+          }}
+          options={dataClass}
+          optionLabel="description"
+          placeholder="בחר כיתה"
+          className="p-invalid w-full md:w-14rem"
+          style={{ marginLeft: "3%", marginRight: "3%" ,width:"15%"}}
+        />
+      )}
+      <Dropdown
+        disabled={flagClass}
+        value={selectedSubject}
+        onChange={(e) => {
+          setSelectedSubject(e.value);
+          setflagSubject(false);
+        }}
+        options={arrsubjects}
+        optionLabel="description"
+        placeholder="בחר מקצוע"
+        className="p-invalid w-full md:w-14rem"
+        style={{ marginLeft: "3%", marginRight: "3%" ,width:"15%"}}
+      />
+      <Dropdown
+        disabled={flagSubject}
+        value={selectedSubsubject}
+        onChange={(e) => {
+          setSelectedSubsubject(e.value);
+          setflagSubsubject(false);
+        }}
+        options={arrsubsubjects}
+        optionLabel="description"
+        placeholder="בחר נושא"
+        className="p-invalid w-full md:w-14rem"
+        style={{ marginLeft: "3%", marginRight: "3%",width:"15%" }}
+      />
+      <Dropdown
+        disabled={flagSubsubject}
+        value={selectedlevel}
+        onChange={(e) => {
+          setSelectedlevel(e.value);
+          setf(true);
+          idlevel = e.value.idlevel;
+        }}
+        options={arrlevels}
+        optionLabel="description"
+        placeholder="בחר רמה"
+        className="p-invalid w-full md:w-14rem"
+        style={{ marginLeft: "3%", marginRight: "3%" ,width:"15%"}}
+      />
+      {/* {f && <GetPageQ idlevel={idlevel}></GetPageQ>} */}
+    </div>
+  );
 }
-    
 
 // export default function Select() {
 //     const [value, setValue] = useState('');
@@ -99,7 +226,7 @@ export default function SelectMaterial() {
 //             <AutoComplete style={{"marginLeft":"2%","marginRight":"2%"}} value={value4} suggestions={items4} completeMethod={search4}  placeholder="בחר רמה" onChange={(e) => setValue4(e.value)} dropdown />
 
 //         </div>
-        
+
 //     )
 // }
 // import React, { useState } from "react";
@@ -142,15 +269,14 @@ export default function SelectMaterial() {
 
 //     return (
 //         <div className="card flex justify-content-center">
-//             <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name" 
+//             <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name"
 //                 placeholder="בחר כיתה" className="w-full md:w-14rem" style={{"marginLeft":"3%","marginRight":"3%"}}/>
-//              <Dropdown value={selectedCity2} onChange={(e) => setSelectedCity2(e.value)} options={cities2} optionLabel="name" 
-//                 placeholder="בחר מקצוע" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>   
-//             <Dropdown value={selectedCity3} onChange={(e) => setSelectedCity3(e.value)} options={cities3} optionLabel="name" 
-//                 placeholder="בחר נושא" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>   
-//             <Dropdown value={selectedCity4} onChange={(e) => setSelectedCity3(e.value)} options={cities4} optionLabel="name" 
-//                 placeholder="בחר רמה" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>  
+//              <Dropdown value={selectedCity2} onChange={(e) => setSelectedCity2(e.value)} options={cities2} optionLabel="name"
+//                 placeholder="בחר מקצוע" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>
+//             <Dropdown value={selectedCity3} onChange={(e) => setSelectedCity3(e.value)} options={cities3} optionLabel="name"
+//                 placeholder="בחר נושא" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>
+//             <Dropdown value={selectedCity4} onChange={(e) => setSelectedCity3(e.value)} options={cities4} optionLabel="name"
+//                 placeholder="בחר רמה" className="w-full md:w-14rem" style={{"marginLeft":"3%" }}/>
 //         </div>
 //     )
 // }
-
--- client/src/Hooks/useGetData.js
+++ client/src/Hooks/useGetData.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-//import useAxios from 'axios-hooks'
-export default async function useGetData(url)
-{
+import useAxios from 'axios-hooks'
 
-  // const [{ data, loading, error }, refetch] = useAxios(url);
-  // useEffect(() => { console.log("error", error); }, [error]);
-  // return { data, loading, error, refetch }
-  // const [{ data, loading, error }, refetch] = useAxios(
-  //   `${url}/${param}`)
-    try {
-        const response = await axios.get(url);
-        console.log(response.data);
-        return response.data;
-      } catch (error) {
-        console.log(error);
-      }
+ const useGetData=(url)=> {
+  const [{ data, loading, error }, refetch] = useAxios(url);
+  useEffect(() => { console.log("error", error); }, [error]);
+  return { data, loading, error, refetch }
+  // try {
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  //   return response.data;
+  // } catch (error) {
+  //   console.log(error);
+  // }
 }
+export default useGetData

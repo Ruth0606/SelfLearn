19aa6f35f5b72e465db0447eb4935793485bca53
ruthychaya <36213877350@mby.co.il>
2023-03-20 19:45:47 +0200

    11

--- client/src/Components/Tirgul/GetPageQ.js
+++ client/src/Components/Tirgul/GetPageQ.js
@@ -6,8 +6,10 @@ import useGetData from "../../Hooks/useGetData";
 import { useDataFunctions } from "../../Hooks/useDataFunctions";
 
 export default function GetPageQ(props) {
-  var dataAnswer = [];
-  var data = [];
+  // var dataAnswer = [];
+  // var data = [];
+  const [dataAnswer, setdataAnswer] = useState([]);
+  const [data, setdata] = useState([]);
 
   console.log("props.idlevel", props.idlevel);
   const { getDataFunc } = useDataFunctions();
@@ -20,21 +22,27 @@ export default function GetPageQ(props) {
   // } = useGetData(`http://localhost:8000/question/1/${props.idlevel}`);
   useEffect(()=>{console.log("flag",flag)},flag)
 useEffect(()=>{
+  console.log("2222222222222222222222222222222222222222222222222222222222222222222")
   getDataFunc(`http://localhost:8000/question/1/${props.idlevel}`).then(
     (data1) => {
-    data=data1
+    setdata(data1);
       data1.forEach((element) => {
         const idquestion = element.idquestion;
         getDataFunc(
           `http://localhost:8000/answer/byidquestion/${idquestion}`
         ).then((data2) => {
-          dataAnswer.push(data2);
+          //dataAnswer.push(data2);
+          // let arr=dataAnswer
+          // arr.push(data2)
+          console.log({data2})
+          setdataAnswer(dataAnswer)
+          console.log({dataAnswer})
         });
       });
       console.log("dataAnswer", dataAnswer)
       setflag(true);
       console.log("ffffffff", flag);
-
+      console.log(dataAnswer.length)
     }
   );
 },[])
@@ -60,13 +68,14 @@ useEffect(()=>{
   //   }
   // }, [data]);
 
-  var k = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  var k = [1, 2]//, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   return (
-
-    flag && <div style={{ margin: "3%", textAlign: "right" }}>
+    
+    dataAnswer.length>0 && <div style={{ margin: "3%", textAlign: "right" }}>
       {k.map((val) => {
         console.log("dataAnswer in return", dataAnswer)
+        console.log("data in return", data)
         console.log("111111", data[val - 1]);
         console.log("2222222", dataAnswer[val - 1]);
         console.log("333", val);
--- client/src/Components/Tirgul/TirgulQ.js
+++ client/src/Components/Tirgul/TirgulQ.js
@@ -8,27 +8,21 @@ export default function TirgulQ(props) {
     // const { data, error, loading, refetch } = useGetData(
     //     `http://localhost:8000/question/${props.type}/${props.idlevel}`
     //   );
+
     
-    // const categories = [
-    //     { name: 'Accounting', key: 'A' },
-    //     { name: 'Marketing', key: 'M' },
-    //     { name: 'Production', key: 'P' },
-    //     // { name: 'Research', key: 'R' }
-    // ];
-    console.log("props.categories",props.categories)
+    console.log("props.categories", props.categories)
     const [selectedCategory, setSelectedCategory] = useState(props.categories[0]);
 
     return (
         <div className="card flex justify-content-center">
             <div className="flex flex-column gap-3" >
-            <span style={{"marginRight":"2%" }}>שאלה {props.num}:</span><br></br><br></br>
-            <span style={{"margin":"2%" }}>{props.quest.description}= </span>
+                <span style={{ "marginRight": "2%" }}>שאלה {props.num}:</span><br></br><br></br>
+                <span style={{ "margin": "2%" }}>{props.quest.description}= </span>
                 {props.categories.map((category) => {
                     return (
-                        
-                        <div key={category.idquestion} className="flex align-items-center"  style={{"margin":"3%" }}>
-                            <RadioButton inputId={category.idquestion} name="category" value={category} onChange={(e) => setSelectedCategory(e.value)} checked={selectedCategory.idquestion === category.idquestion} />
-                            <label htmlFor={category.idquestion} className="ml-2">{category.description}</label>
+                        <div key={category.idanswer} className="flex align-items-center" style={{ "margin": "3%" }}>
+                            <RadioButton inputId={category.idquestion} name="category" value={category} onChange={(e) => setSelectedCategory(e.value)} checked={selectedCategory.idanswer === category.idanswer} />
+                            <label htmlFor={category.idanswer} className="ml-2">   {category.description}   </label>
                         </div>
                     );
                 })}

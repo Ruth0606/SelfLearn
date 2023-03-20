6f04e33331510ef718cb326cbd0c60c061c3a1d8
chayaruthi <36325569028@mby.co.il>
2023-03-20 20:50:13 +0200

    fdg

--- client/src/Components/Test/GetMarks.js
+++ client/src/Components/Test/GetMarks.js
@@ -8,15 +8,6 @@ export default function GetMarks() {
     const [data1, setData1] = useState([]);
     const { getDataFunc } = useDataFunctions();
 
-    // const {
-    //     data,
-    //     error,
-    //     loading,
-    //     refetch,
-    //   } = useGetData(`http://localhost:8000/question/test/byStudent?idstudent=1`);
-
-    //  useEffect(()=>{  console.log(data)},[data])
-
     useEffect(() => {
         // getDataFunc(`http://localhost:8000/question/test/getSubjectsTestsByIdStudent?idstudent=1`)
         //     .then(
@@ -35,15 +26,19 @@ export default function GetMarks() {
             .then(
                 (data) => {
                     // console.log("data")
-                    setData(data);
-                    console.log({data});
-                    console.log("/////////////////")
-                    console.log(data[0].level.subsubject.subject.idsubject)
-                    console.log("/////////////////")
-                    const test2 = [...data].sort(
-                        (a2, b2) => 
-                        a2.level.subsubject.subject.idsubject- b2.level.subsubject.subject.idsubject);
-                    console.log({ test2 })
+                    const x="level.subsubject.subject.idsubject"
+                    // console.log({data});
+                    // console.log("/////////////////")
+                    // console.log(datayt[0][x]+"chavi")
+                    // console.log("/////////////////")
+                    // const test2 = [...data].sort(
+                    //     (a2, b2) => 
+                        // a2.level.subsubject.subject.idsubject- b2.level.subsubject.subject.idsubject);
+                    // console.log({ test2 })
+                    const numAscending = [...data].sort((a, b) => a[x] - b[x]);
+                    console.log(numAscending)
+                    setData(numAscending);
+
                 }
             );
 
@@ -62,9 +57,30 @@ export default function GetMarks() {
     //       );
     //     }
     //   }, [selectedSubject]);
-
+    const a="level.subsubject.subject.description";
+    const b="level.subsubject.subject.idsubject";
     return (
         <div>
+            {data&&
+            data.map((item,i)=>{
+                return(
+                    <>
+                <b>{item[a]}</b>
+                {item[b]!==data[i-1][b]&&
+                getDataFunc(`http://localhost:8000/question/test/getSubjectsTestsByIdStudent?idstudent=1`)
+                    .then(
+                        (data1) => {
+                           
+                        }
+                    )
+
+                }
+
+
+                    </>
+                )
+
+            })}
             <h1>ציונים</h1>
             <button onClick={() => {
                 // const t = [1, 2, 1, 3, 4, 1].sort((a, b) => a - b);

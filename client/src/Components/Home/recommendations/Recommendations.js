41a37348d5a4033d6fd95835c5a6380edd603b6c
chayaruthi <36325569028@mby.co.il>
2023-05-04 18:54:57 +0300

    vn

--- client/src/Components/Home/recommendations/Recommendations.js
+++ client/src/Components/Home/recommendations/Recommendations.js
@@ -85,14 +85,12 @@ export default function Recommendations(props) {
     const productTemplate = (product) => {
         return (
             <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
-                <div className="mb-3">
-                    <img src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} className="w-6 shadow-2" />
-                </div>
                 <div>
                     <h4 className="mb-1">{product.name}</h4>
                     {/* <h6 className="mt-0 mb-3">${product.price}</h6> */}
                     {/* <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag> */}
                     <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
+                        {/* {p} */}
                         {/* <Button icon="pi pi-search" className="p-button p-button-rounded" />
                         <Button icon="pi pi-star-fill" className="p-button-success p-button-rounded" /> */}
                     </div>
@@ -107,8 +105,8 @@ export default function Recommendations(props) {
 
   
         <div className="card">
-            <Carousel value={products} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} className="custom-carousel" circular
-            autoplayInterval={3000} itemTemplate={productTemplate} />
+            <Carousel value={products} numVisible={1} numScroll={1} responsiveOptions={responsiveOptions} className="custom-carousel" circular
+            autoplayInterval={3000} itemTemplate={productTemplate} style={{ direction: "ltr"}}/>
         </div>
 
 
